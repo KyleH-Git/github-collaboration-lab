36311502186d9aa689671c2dfa8e938a901db137
Kyle Hayden <dahaydnator@gmail.com>
2025-04-11 13:45:47 -0500

    added Header2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from './Components/Header/Header.jsx';
+import Header2 from './Components/Header2/Header2.jsx';
 <Header />
 function App() {
  
   return (
     <>
     <Header />
+    <Header2 />
   <div>Hello World!</div>
   <Form> </Form>
   </>
---
+++ src/Components/Header2/Header2.jsx
@@ -0,0 +1,11 @@
+const Header2 = (props) => {
+    return (
+        <>
+            <header>
+                <h1>Header2!</h1>
+            </header>
+        </>
+    );
+};
+
+export default Header2;
