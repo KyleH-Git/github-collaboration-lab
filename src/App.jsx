34f857cd0d6c2d28847f5710cdc40edc38df267c
Kyle Hayden <dahaydnator@gmail.com>
2025-04-11 13:35:04 -0500

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
-import Header from '../components/Header/Header.jsx'
+import Header from './Components/Header/Header.jsx';
+<Header />
 function App() {
  
   return (
